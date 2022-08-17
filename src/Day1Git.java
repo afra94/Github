58f2cb16ff3e510585aba6e59f90671566f4a0a7
afra94 <kolotafra@gmail.com>
2022-08-17 23:59:32 +0300

    V1

---
+++ src/Day1Git.java
@@ -0,0 +1,7 @@
+public class Day1Git {
+    public static void main(String[] args) {
+        System.out.println("hello git 1.deneme");
+        System.out.println("hello git 2.deneme");
+        System.out.println("hello git 3.deneme");
+    }
+}
